b6b1c5a6377721b506cae3cdbcb390369daff802
aMpujari <32033126+aMpujari@users.noreply.github.com>
2019-01-22 23:47:44 +0530

    first commit
    demo spring boot app

---
+++ src/main/java/com/example/mySpringBootProject/HelloController.java
@@ -0,0 +1,32 @@
+package com.example.mySpringBootProject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author pc1
+ */
+@Controller
+public class HelloController {
+      
+  
+    
+      @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index() {
+        return "welcome";
+    }
+    
+ 
+
+
+    
+}
---
+++ src/main/webapp/WEB-INF/welcome.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : welcome
+    Created on : 22 Jan, 2019, 11:21:38 PM
+    Author     : HP USER
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Welcome Pallavi!</title>
+    </head>
+    <body>
+        <h1>I Love You Pallavi!!!! - ${name}</h1>
+    </body>
+</html>
